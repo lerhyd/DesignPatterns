870532eb800258adda09a3bac91fde76ce882353
lerhyd <lerhyd@gmail.comm>
2020-05-07 19:34:57 +0300

    add singlton implementation

---
+++ Creational/SingletonApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SingletonApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton1 = FileWorkerSingleton.Instanse;
+            var singleton2 = FileWorkerSingleton.Instanse;
+
+            //var singleton = new FileWorkerSingleton();
+
+            singleton1.WriteText("Hello, World!");
+            singleton2.WriteText("Hi, Bro!");
+
+            singleton1.Save();
+            singleton2.Save();
+        }
+    }
+}
